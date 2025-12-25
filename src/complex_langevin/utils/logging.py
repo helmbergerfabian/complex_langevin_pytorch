073b229187db45b8926bbc2266be40365dfc4f34
helmbergerfabian <helmbergerfabian@gmail.com>
2025-12-25 14:34:23 +0100

    Logger class

---
+++ src/complex_langevin/utils/logging.py
@@ -0,0 +1,15 @@
+# complex_langevin/utils/logging.py
+class Logger:
+    def log(self, message):
+        self._sender = None
+        pass
+
+    def _log_enabled(self, message):
+        print(f"[{self._sender}] {message}")
+
+    def _log_disabled(self, message):
+        pass
+
+    def _init_logger(self, verbose: bool, sender: str = "Logger"):
+        self._sender = sender
+        self.log =  self._log_enabled if verbose else self._log_disabled

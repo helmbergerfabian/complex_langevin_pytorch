6831d3431d4735ac3f662b1c5df0b5d48659c6bd
helmbergerfabian <helmbergerfabian@gmail.com>
2025-12-29 23:26:25 +0100

    proper moment for unmod model

--- src/complex_langevin/models/zero_d_scalar_phi4.py
+++ src/complex_langevin/models/zero_d_scalar_phi4.py
@@ -21,3 +21,9 @@ class SimModel(SimLogger):
         y = np.exp(-self.action(x))
         integral = np.trapezoid(y=y, x=x)
         return integral
+    
+    def moment(self, order):
+        x = np.linspace(-4, 4, 1000)
+        y = np.exp(-self.action(x)) * x**order
+        integral = np.trapezoid(y=y, x=x) / self.partition_function()
+        return integral
